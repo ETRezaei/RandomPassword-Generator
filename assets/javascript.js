79d576df59b015022d2cd62dd9a954943eb6c1c2
Emi Rezaei <tomoyose.emi@gmail.com>
2019-09-19 23:08:54 -0700

    random generator completed

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,11 +1,11 @@
-// function getRandomPass(){
+function getRandomPass(){
     let userPassLength = prompt("Please pick a password length between 8 to 128");
     if(userPassLength > 7 && userPassLength <= 128){
         
-        let questions = [{ q: "Do you want 'Special Character'?", a:false, data: "~!@-#$" },
+        let questions = [{ q: "Do you want 'Special Character'?", a: false, data: "~!@-#$" },
                         { q: "Do you want 'Numeric Character'?", a: false, data: "0123456789" },
-                        { q: "Do you want 'Lowercase Character'?", a:false, data: "abcdefghijklmnopqrstuvwxyz" },
-                        { q: "Do you want 'Upercase Character'?", a:false, data: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}]
+                        { q: "Do you want 'Lowercase Character'?", a: false, data: "abcdefghijklmnopqrstuvwxyz" },
+                        { q: "Do you want 'Upercase Character'?", a: false, data: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}]
 
 
         let password = '';
@@ -21,18 +21,54 @@
                 source = source + questions[i].data;
             }
         }
+
+        if (source === ""){
+            alert("please choose at least one character type!");
+        }
+        else{
         for(let c = 0; c < userPassLength; c++){
            let i = Math.floor(Math.random()*source.length);
             password = password + source[i];
-            console.log(password);
+            // console.log(password);
         }
-        console.log(password);
+        // console.log(password);
   
         //if(questions[0].q
+        return password;
+    }
     }
     else{
         alert("Your input is not valid. Please try again!!")
     }
     
+}
 
-// }
+let request = document.querySelector("#generate");
+let textArea = document.querySelector("#password");
+let copy = document.querySelector("#copy");
+
+request.addEventListener('click',function(event){
+    event.preventDefault();
+    let finalPass = getRandomPass();
+
+    textArea.textContent = finalPass;
+    copy.disabled = false;
+
+})
+
+copy.addEventListener("click", function(){
+    copyToClipboard();
+})
+
+
+function copyToClipboard() {
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.select();
+    document.execCommand("copy");
+  
+    alert(
+      "Your password " + passwordText.value + " was copied to your clipboard."
+    );
+  }
+
